be027c50eae97d2e68da68c1d347a7da71e0570a
Krasi Marinov <kmarinov@melontech.com>
2020-08-30 22:48:27 +0300

    jenkins keys

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -23,6 +23,7 @@ sudo chkconfig docker on
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
